bc80718383855dfd38deb847de786b9de8138896
Sriyan Nakka <sriyannakka@gmail.com>
2025-09-24 11:46:29 +0530

    version 0.2
    replaced static SVG img assets for animated, added index.html and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const playBtn = document.getElementById("playButton");
+
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const num3 = document.getElementById("num3");
+const num4 = document.getElementById("num4");
+const num5 = document.getElementById("num5");
